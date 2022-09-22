c404dd32d68cdb1dd7cee0fa9ce98c50bfc2dd73
iamwhitegod <kmbagwu12@gmail.com>
2022-09-22 10:21:54 +0100

    Log to console

--- server.cjs
+++ server.cjs
@@ -8,4 +8,4 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(9000);
+app.listen(9000, () => console.log("Listening on port 9000"));
